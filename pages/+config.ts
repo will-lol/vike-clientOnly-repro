8cab7fa98ba0c58dbe5f1b3695d661b0d5e6bbd7
will <will.bradshaw50@gmail.com>
2025-12-16 09:44:32 +1100

    feat: photon support

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "vike/types";
 import vikeReact from "vike-react/config";
+import vikePhoton from "vike-photon/config";
 
 // Default config (can be overridden by pages)
 // https://vike.dev/config
@@ -9,5 +10,5 @@ export default {
   title: "My Vike App",
   description: "Demo showcasing Vike",
 
-  extends: [vikeReact],
+  extends: [vikeReact, vikePhoton],
 } satisfies Config;
