b76f588426cba77363271cdaf51503142f7ba61f
Bati <no-reply@batijs.dev>
2025-12-16 08:28:39 +1100

    scaffold Vike app with Bati

---
+++ pages/+config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "vike/types";
+import vikeReact from "vike-react/config";
+
+// Default config (can be overridden by pages)
+// https://vike.dev/config
+
+export default {
+  // https://vike.dev/head-tags
+  title: "My Vike App",
+  description: "Demo showcasing Vike",
+
+  extends: [vikeReact],
+} satisfies Config;
