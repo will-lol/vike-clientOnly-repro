6eedd8de7e344d4ecf9c97804cb932bc593cb77a
will <will.bradshaw50@gmail.com>
2025-12-16 09:37:43 +1100

    feat: add repro

---
+++ components/AnimatedLoader.tsx
@@ -0,0 +1,5 @@
+export default function AnimatedLoader() {
+  return (
+    <div className="animate-[ping_1s_cubic-bezier(0,0,0.2,1)_1_normal_forwards] size-4 bg-red-500"></div>
+  );
+}
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,16 +1,107 @@
-import { Counter } from "./Counter.js";
+import { clientOnly } from "vike-react/clientOnly";
+import AnimatedLoader from "../../components/AnimatedLoader";
+import { ClientOnly as ClientOnlyTanstack } from "../../components/ClientOnlyTanstack";
+import HeavyComponent from "../../components/HeavyComponent";
+import { ReactNode, Suspense, use, useState } from "react";
+
+const HeavyComponentClientOnly = clientOnly(
+  () => import("../../components/HeavyComponent"),
+);
 
 export default function Page() {
+  const [stage, setStage] = useState<ClientOnlyDemoProps["stage"]>("server");
+
   return (
-    <>
-      <h1>My Vike app</h1>
-      <p>This page is:</p>
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
+    <main className="prose">
+      <h1>React clientOnly reproduction</h1>
+      <h2>Example 1: vike clientOnly</h2>
+      <HeavyComponentClientOnly
+        fallback={<AnimatedLoader />}
+      ></HeavyComponentClientOnly>
+      <h2>
+        Example 2: Tanstack Router ClientOnly component. No suspense or lazy is
+        required.
+      </h2>
+      <ClientOnlyTanstack fallback={<AnimatedLoader />}>
+        <HeavyComponent />
+      </ClientOnlyTanstack>
+      <h2>
+        Example 3: Demonstration that adding Suspense causes deletion and
+        recreation of fallback
+      </h2>
+      <label htmlFor="stage-select">Select a stage:</label>
+      <select
+        id="stage-select"
+        value={stage}
+        className="border"
+        onChange={(e) => setStage(e.currentTarget.value)}
+      >
+        <option value="server">
+          clientOnly component as it was rendered on the server
+        </option>
+        <option value="clientWhileHydrating">
+          clientOnly component while it is hydrating
+        </option>
+        <option value="clientAfterEffect">
+          clientOnly component after it has mounted and the effect has run
+        </option>
+      </select>
+      <ClientOnlyDemo stage={stage} fallback={<AnimatedLoader />}>
+        <HeavyComponent />
+      </ClientOnlyDemo>
+    </main>
   );
+}
+
+interface ClientOnlyDemoProps {
+  stage: "clientAfterEffect" | "clientWhileHydrating" | "server";
+  fallback: ReactNode;
+  children: ReactNode;
+}
+
+function ClientOnlyDemo({ stage, fallback, children }: ClientOnlyDemoProps) {
+  const { promise, resolve } = useSuspender();
+
+  switch (stage) {
+    case "server":
+      return fallback;
+    case "clientWhileHydrating":
+      return fallback;
+    case "clientAfterEffect":
+      return (
+        <>
+          <button className="border" onClick={() => resolve()}>
+            Resolve suspense
+          </button>
+          <Suspense fallback={fallback}>
+            <SuspendChildren promise={promise}>{children}</SuspendChildren>
+          </Suspense>
+        </>
+      );
+  }
+}
+
+function useSuspender() {
+  const [lock] = useState(() => {
+    let internalResolve: (value: void | PromiseLike<void>) => void = () => {};
+
+    const promise = new Promise<void>((resolve) => {
+      internalResolve = resolve;
+    });
+
+    return { promise, resolve: internalResolve };
+  });
+
+  return lock;
+}
+
+function SuspendChildren({
+  promise,
+  children,
+}: {
+  promise: Promise<void>;
+  children?: ReactNode;
+}) {
+  use(promise);
+  return children;
 }
